48952a583ea9a8497752c6af0bfd5969f47f5d5a
Jeff Lynch <jlynch3@uw.edu>
2021-10-07 14:37:28 -0700

    first commit

--- react-portfolio/src/components/work.js
+++ react-portfolio/src/components/work.js
@@ -9,7 +9,7 @@ export default function Work () {
             Work
         </a>
         <div class="container">
-            <div class="card" style="width: 18rem;">
+            <div class="card" style={{width: "18rem"}}>
                 <img src="./images/Code.jpg" class="card-img-top" alt="..."/>
                 <div class="card-body">
                     <h5 class="card-title">Daily Calendar</h5>
@@ -17,7 +17,7 @@ export default function Work () {
                         <a href="https://kingami34.github.io/Daily_Calendar/" class="btn btn-primary">Preview</a>
                 </div>
             </div>
-            <div class="card1" style="width: 18rem;">
+            <div class="card1" style={{width: "18rem"}}>
                 <img src="./images/Coding.jpg" class="card-img-top" alt="..."/>
                 <div class="card-body">
                     <h5 class="card-title">Code Challenge</h5>
@@ -25,7 +25,7 @@ export default function Work () {
                     <a href="https://kingami34.github.io/Code_Challenge/" class="btn btn-primary">Preview</a>
                 </div>
             </div>
-            <div class="card" style="width: 18rem;">
+            <div class="card" style={{width: "18rem"}}>
                 <img src="./images/Hardwork.jpg" class="card-img-top" alt="..."/>
                 <div class="card-body">
                     <h5 class="card-title">Fixed 2.0</h5>
@@ -33,7 +33,7 @@ export default function Work () {
                     <a href="https://kingami34.github.io/Fixed_2.0/" class="btn btn-primary">Preview</a>
                 </div>
             </div>
-            <div class="card1" style="width: 18rem;">
+            <div class="card1" style={{width: "18rem"}}>
                 <img src="https://images.pexels.com/photos/2603464/pexels-photo-2603464.jpeg?cs=srgb&dl=pexels-aleksandar-pasaric-2603464.jpg&fm=jpg"
                     class="card-img-top" alt="..."/>
                 <div class="card-body">
@@ -42,7 +42,7 @@ export default function Work () {
                     <a href="https://kingami34.github.io/README/" class="btn btn-primary">Preview</a>
                 </div>
             </div>
-            <div class="card" style="width: 18rem;">
+            <div class="card" style={{width: "18rem"}}>
                 <img src="https://images.pexels.com/photos/1772123/pexels-photo-1772123.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
                     class="card-img-top" alt="..."/>
                 <div class="card-body">
