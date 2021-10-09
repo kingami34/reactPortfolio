9f8ca89a2caee28a45441c8701e751bd2bd55345
Jeff Lynch <jlynch3@uw.edu>
2021-10-08 19:22:01 -0700

    first commit

---
+++ src/components/work.js
@@ -0,0 +1,60 @@
+import React from "react";
+import Coding from "./images/Coding.jpg"
+import Code from './images/Code.jpg'
+import Hardwork from './images/Hardwork.jpg'
+
+export default function Work () {
+    return (
+        <body>
+        <div class="list-groups">
+        <a href="#Work" class="list-group-item list-group-item-action active" id="work">
+            Work
+        </a>
+        <div class="container">
+            <div class="card" style={{width: "18rem"}}>
+                <img src=  {Code} class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <h5 class="card-title">Daily Calendar</h5>
+                        <p class="card-text">The Daily Calendar helps the user plan their day.</p>
+                        <a href="https://kingami34.github.io/Daily_Calendar/" class="btn btn-primary">Preview</a>
+                </div>
+            </div>
+            <div class="card1" style={{width: "18rem"}}>
+                <img src= {Coding} class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <h5 class="card-title">Code Challenge</h5>
+                    <p class="card-text">Code Challenge is an app for the user to test their coding knowledge.</p>
+                    <a href="https://kingami34.github.io/Code_Challenge/" class="btn btn-primary">Preview</a>
+                </div>
+            </div>
+            <div class="card" style={{width: "18rem"}}>
+                <img src= {Hardwork} class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <h5 class="card-title">Fixed 2.0</h5>
+                    <p class="card-text">Fixed 2.0 is about refactoring an app.</p>
+                    <a href="https://kingami34.github.io/Fixed_2.0/" class="btn btn-primary">Preview</a>
+                </div>
+            </div>
+            <div class="card1" style={{width: "18rem"}}>
+                <img src="https://images.pexels.com/photos/2603464/pexels-photo-2603464.jpeg?cs=srgb&dl=pexels-aleksandar-pasaric-2603464.jpg&fm=jpg"
+                    class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <h5 class="card-title">ReadMe</h5>
+                    <p class="card-text">With Readme you can generate own readme file.</p>
+                    <a href="https://kingami34.github.io/README/" class="btn btn-primary">Preview</a>
+                </div>
+            </div>
+            <div class="card" style={{width: "18rem"}}>
+                <img src="https://images.pexels.com/photos/1772123/pexels-photo-1772123.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <h5 class="card-title">shelfLife</h5>
+                    <p class="card-text">Shelf Life is an app that helps the user organize their refrigerator.</p>
+                    <a href="https://shelf-life-206.herokuapp.com/" class="btn btn-primary">Preview</a>
+                </div>
+            </div>
+        </div>
+        </div>
+        </body>
+    );
+}
